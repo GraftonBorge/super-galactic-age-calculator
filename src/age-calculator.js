f07157f13627c1a15d4d2d2a96e2ecb1fcc00e4a
GraftonBorge <graftonmborge@gmail.com>
2021-07-30 15:35:59 -0700

    pass third test

--- src/age-calculator.js
+++ src/age-calculator.js
@@ -3,5 +3,9 @@ export default class ageCalculator {
   constructor(earthYearAge) {
     this.earthYearAge = earthYearAge
     this.earthDayAge = earthYearAge * 365
+    this.marsYearAge = Math.trunc(this.earthDayAge / 687)
   }
+
+
+
 }
