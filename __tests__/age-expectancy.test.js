3b3a40b7fed8c5291752958b15c07e0bbf1a989d
GraftonBorge <graftonmborge@gmail.com>
2021-07-30 19:18:38 -0700

    pass first test for age_expectancy

--- __tests__/age-expectancy.test.js
+++ __tests__/age-expectancy.test.js
@@ -4,11 +4,10 @@ import demographic from "../src/age-expectancy";
 
 describe('demographic', () => {
   let maleDemographic;
-  let femaleDemographic;
 
   beforeEach(() => {
     maleDemographic = new demographic("male");
-    femaleDemographic = new demographic("female")
+
   })
 
   test('should return life expectancy for my male demographic', () => {
--- src/age-expectancy.js
+++ src/age-expectancy.js
@@ -1,9 +1,29 @@
 
 export default class demographic {
 
-  constructor() {
+  constructor(gender, countrie) {
+    this.gender = gender
+    this.countrie = countrie
+    this.lifeExpectancy;
   }
 
-
+maleAgeExpectancy(countrieChosen) {
+  const db_male = [
+  {
+    countrie: "USA",
+    lifeExpectancy: 75
+  },
+  {
+    countrie: "UK",
+    lifeExpectancy: 79
+  }]
+  for (let i = 0; i < db_male.length; i++) {
+    let currentCountrie = db_male[i];
+    if (currentCountrie.countrie === countrieChosen) {
+      this.countrie = countrieChosen;
+      this.lifeExpectancy = currentCountrie.lifeExpectancy;
+    }
+  }
+}
 };
 
